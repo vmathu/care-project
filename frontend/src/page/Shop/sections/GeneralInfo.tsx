75d26a7cf5f1a03362e7226f7df2f6bf0cb506ee
Ngan Truc <ngantruc2003@gmail.com>
2023-11-20 12:07:07 +0700

    add rating section

--- frontend/src/page/Shop/sections/GeneralInfo.tsx
+++ frontend/src/page/Shop/sections/GeneralInfo.tsx
@@ -1,7 +1,17 @@
+import React from "react";
 import { makeStyles } from "@mui/styles";
-import data from "../mocks/GeneralInfo.json";
-import color from "libs/ui/color";
 import { Typography, Rating, Button } from "@mui/material";
+import {
+  LocationOnRounded,
+  LocalPhoneRounded,
+  AccessTimeRounded,
+  AttachMoneyRounded,
+  LocalParkingRounded,
+} from "@mui/icons-material";
+
+import color from "libs/ui/color";
+
+import data from "../mocks/GeneralInfo.json";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -33,6 +43,15 @@ const useStyles = makeStyles(() => ({
     gap: "1rem",
     alignItems: "center",
   },
+  infoList: {
+    display: "flex",
+    flexDirection: "column",
+    gap: ".5rem",
+  },
+  info: {
+    display: "flex",
+    gap: "1rem",
+  },
 }));
 
 const TagList = () => {
@@ -49,8 +68,45 @@ const TagList = () => {
   );
 };
 
+type InfoProps = {
+  icon: React.ReactNode;
+  title: string;
+};
+
+const infoList: InfoProps[] = [
+  {
+    icon: <LocationOnRounded />,
+    title: `${data.address.street}, ${data.address.district}, ${data.address.city}`,
+  },
+  {
+    icon: <LocalPhoneRounded />,
+    title: `${data.phone}`,
+  },
+  {
+    icon: <AccessTimeRounded />,
+    title: `${data.time.open} - ${data.time.close}`,
+  },
+  {
+    icon: <AttachMoneyRounded />,
+    title: `~${data.price}`,
+  },
+  {
+    icon: <LocalParkingRounded />,
+    title: `${data.parking === true ? "Miễn phí gửi xe" : "Phí gửi xe riêng"}`,
+  },
+];
+
 export default function GeneralInfo() {
   const classes = useStyles();
+
+  const total = data.rate.reduce(
+    (previousValue, currentValue) => previousValue + currentValue
+  );
+  const sum =
+    data.rate.reduce(
+      (previousValue, currentValue, currentIndex) =>
+        previousValue + currentValue * (currentIndex + 1)
+    ) / total;
   return (
     <div className={classes.root}>
       <TagList />
@@ -58,18 +114,31 @@ export default function GeneralInfo() {
         <div>
           <Typography variant="h4">{data.name}</Typography>
           <div className={classes.rate}>
-            <Typography variant="caption">{data.rate}/5</Typography>
-            <Rating
-              size="small"
-              readOnly
-              defaultValue={data.rate}
-              precision={0.1}
-            />
+            <Typography variant="caption">
+              {Math.floor(sum * 10) / 10}/5
+            </Typography>
+            <Rating size="small" readOnly defaultValue={sum} precision={0.1} />
             <Typography variant="caption">{data.bookings} lượt đặt</Typography>
           </div>
         </div>
         <Button variant="text">Report</Button>
       </div>
+      <div className={classes.infoList}>
+        {infoList.map((info) => (
+          <div className={classes.info}>
+            {info.icon}
+            <Typography variant="body1">{info.title}</Typography>
+          </div>
+        ))}
+      </div>
+      {/* <div>
+        <Button variant="outlined">2 chỗ</Button>
+        <Button variant="outlined">4 chỗ</Button>
+        <Button variant="outlined">10 chỗ</Button>
+      </div>
+      <Button variant="contained" size="large">
+        ĐẶT NGAY
+      </Button> */}
     </div>
   );
 }
