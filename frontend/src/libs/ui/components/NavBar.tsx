ee17946d88b14ef5adf40f1623fb66a329e8a37f
vmathu <anhthuvominh1@gmail.com>
2023-11-21 14:14:45 +0700

    CARE-16 clean code

--- frontend/src/libs/ui/components/NavBar.tsx
+++ frontend/src/libs/ui/components/NavBar.tsx
@@ -1,52 +1,13 @@
-import React, { useState, FormEvent } from "react";
-import {
-  AppBar,
-  Toolbar,
-  TextField,
-  Box,
-  useTheme,
-  useMediaQuery,
-  styled,
-  InputAdornment,
-} from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import { useNavigate, Link } from "react-router-dom";
+import { AppBar, Toolbar, Box, useTheme, useMediaQuery } from "@mui/material";
+
 import LogoText from "../../../assets/app-logo-text.svg";
 import Avatar from "./Avatar";
 import colors from "../color";
 import { CustomSearch } from ".";
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: "80px",
-  marginRight: theme.spacing(2),
-  width: "352px",
-  height: "48px",
-  display: "flex",
-  alignItems: "center",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
-
 export const SearchAppBar = ({}) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const navigate = useNavigate();
-
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearchSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    if (searchTerm !== "") {
-      navigate(`/search/${searchTerm}`);
-    }
-  };
 
   return (
     <AppBar
