3cba37197b23aa37ab97198da96b3337b2d3a8e6
nhatminhtrieu <21127112@student.hcmus.edu.vn>
2023-11-24 00:08:39 +0700

    CARE-2 add tab component

--- frontend/src/page/Home/index.tsx
+++ frontend/src/page/Home/index.tsx
@@ -1,9 +1,11 @@
-import { SearchAppBar } from 'libs/ui/components/NavBar'
+import { SearchAppBar, Tab } from 'libs/ui/components'
 
 export default function HomePage() {
+  const tabItems = ['Top đánh giá', 'Gần bạn', 'Yêu thích'];
   return (
     <div>
       <SearchAppBar />
+      <Tab tabItems={tabItems} />
     </div>
   )
 }
