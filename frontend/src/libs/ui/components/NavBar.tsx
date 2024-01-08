011c9c0979075ad35dcc66267902d0f0e9b69acd
Ngan Truc <ngantruc2003@gmail.com>
2024-01-08 19:08:31 +0700

    fix padding section

--- frontend/src/libs/ui/components/NavBar.tsx
+++ frontend/src/libs/ui/components/NavBar.tsx
@@ -6,31 +6,27 @@ import colors from "../color";
 import { CustomSearch } from ".";
 
 export const SearchAppBar = ({}) => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+	const theme = useTheme();
+	const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-  return (
-    <AppBar
-      position="static"
-      style={{ backgroundColor: colors.white }}
-      elevation={0}
-    >
-      <Toolbar style={{ padding: isMobile ? "16px" : "20px 80px" }}>
-        {/* <img src={LogoText} alt="logo" style={{ width: '', height: isMobile ? '20px' : '40px' }} /> */}
-        <img
-          src={LogoText}
-          alt="logo"
-          style={{
-            width: isMobile ? "100px" : "",
-            height: isMobile ? "20px" : "40px",
-            padding: isMobile ? "0 16px" : "",
-          }}
-        />
-        <Box flexGrow={1} />
-        <CustomSearch />
-        <Box mr={2} />
-        <Avatar />
-      </Toolbar>
-    </AppBar>
-  );
+	return (
+		<AppBar position="static" style={{ backgroundColor: colors.white }} elevation={0}>
+			<Toolbar style={{ padding: isMobile ? "12px 16px" : "16px 80px" }}>
+				{/* <img src={LogoText} alt="logo" style={{ width: '', height: isMobile ? '20px' : '40px' }} /> */}
+				<img
+					src={LogoText}
+					alt="logo"
+					style={{
+						width: isMobile ? "100px" : "",
+						height: isMobile ? "20px" : "40px",
+						padding: isMobile ? "0 16px" : "",
+					}}
+				/>
+				<Box flexGrow={1} />
+				<CustomSearch />
+				<Box mr={2} />
+				<Avatar />
+			</Toolbar>
+		</AppBar>
+	);
 };
--- frontend/src/page/Shop/sections/ImagePreviewer.tsx
+++ frontend/src/page/Shop/sections/ImagePreviewer.tsx
@@ -1,38 +1,42 @@
-import { ImageList, ImageListItem, Hidden } from "@mui/material";
+import { ImageList, ImageListItem, Hidden, styled } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { cover, image1, image2, image3, image4 } from "../mocks";
 
+const StyledImageList = styled(ImageList)(({ theme }) => ({
+	margin: "0px",
+}));
+
 const useStyles = makeStyles(() => ({
-  img: {
-    borderRadius: "4px",
-  },
+	img: {
+		borderRadius: "4px",
+	},
 }));
 export default function ImagePreviewer() {
-  const classes = useStyles();
-  return (
-    <div>
-      <Hidden smDown>
-        <ImageList cols={5} gap={16} rowHeight={110} variant="quilted">
-          <ImageListItem cols={5} rows={3}>
-            <img className={classes.img} src={cover} alt="cover" />
-          </ImageListItem>
-          <ImageListItem>
-            <img className={classes.img} src={image1} alt="image1" />
-          </ImageListItem>
-          <ImageListItem>
-            <img className={classes.img} src={image2} alt="image2" />
-          </ImageListItem>
-          <ImageListItem>
-            <img className={classes.img} src={image3} alt="image3" />
-          </ImageListItem>
-          <ImageListItem>
-            <img className={classes.img} src={image4} alt="image4" />
-          </ImageListItem>
-          <ImageListItem>
-            <img className={classes.img} src={cover} alt="image5" />
-          </ImageListItem>
-        </ImageList>
-      </Hidden>
-    </div>
-  );
+	const classes = useStyles();
+	return (
+		<div>
+			<Hidden smDown>
+				<StyledImageList cols={5} gap={16} rowHeight={110} variant="quilted">
+					<ImageListItem cols={5} rows={3}>
+						<img className={classes.img} src={cover} alt="cover" />
+					</ImageListItem>
+					<ImageListItem>
+						<img className={classes.img} src={image1} alt="image1" />
+					</ImageListItem>
+					<ImageListItem>
+						<img className={classes.img} src={image2} alt="image2" />
+					</ImageListItem>
+					<ImageListItem>
+						<img className={classes.img} src={image3} alt="image3" />
+					</ImageListItem>
+					<ImageListItem>
+						<img className={classes.img} src={image4} alt="image4" />
+					</ImageListItem>
+					<ImageListItem>
+						<img className={classes.img} src={cover} alt="image5" />
+					</ImageListItem>
+				</StyledImageList>
+			</Hidden>
+		</div>
+	);
 }
